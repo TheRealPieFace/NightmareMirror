0667181ad608fbf4a9bf0a5d31ea25bf89f10192
TheRealPieFace <34294484+TheRealPieFace@users.noreply.github.com>
2020-10-24 10:12:41 -0400

    Interaction System

---
+++ Assets/_Scripts/Hazard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    public void TriggerActivate()
+    {
+        this.enabled = true;
+    }
+
+    public void InteractActivate()
+    {
+        this.enabled = !this.enabled;
+    }
+}
---
+++ Assets/_Scripts/Interactable.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public enum Interaction
+    {
+        Trigger,
+        Toggle
+    }
+
+    [SerializeField] Hazard hazard;
+    [SerializeField] Interaction interactionType = Interaction.Trigger;
+    private bool inRange = false;
+
+    private void Update()
+    {
+        if(inRange && Input.GetKeyDown(KeyCode.E))
+        {
+            hazard.InteractActivate();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if(interactionType == Interaction.Toggle)
+            {
+                inRange = true;
+                //TODO ACTIVATE PROMPT
+            }
+            else if (interactionType == Interaction.Trigger)
+            {
+                hazard.TriggerActivate();
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (interactionType == Interaction.Toggle)
+            {
+                inRange = false;
+                //TODO DEACTIVATE PROMPT
+            }
+        }
+    }
+
+}
