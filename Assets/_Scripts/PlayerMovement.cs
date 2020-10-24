7792ccb7ffe775e094a13787cacdba82a3003e97
JeanPerez117 <35622770+JeanPerez117@users.noreply.github.com>
2020-10-24 17:51:57 -0400

    player horizontal

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * moveSpeed;
+    }
+}
