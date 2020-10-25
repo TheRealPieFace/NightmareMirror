d6f7c57bff258f9e2ef180adebc70371527cb9c1
TheRealPieFace <34294484+TheRealPieFace@users.noreply.github.com>
2020-10-25 14:35:34 -0400

    ag4we

--- Assets/_Scripts/EndInteraction.cs
+++ Assets/_Scripts/EndInteraction.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class EndInteraction : MonoBehaviour
 {
     [SerializeField] Text prompt;
+    private bool inRange = false;
     public bool active = false;
+    [SerializeField] GameObject endScreen;
+
+    private void Update()
+    {
+        if(inRange && Input.GetKeyDown(KeyCode.E))
+        {
+            Time.timeScale = 0;
+            endScreen.SetActive(true);
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(active && collision.gameObject.tag == "Player")
         {
             prompt.enabled = true;
+            inRange = true;
         }
     }
 
@@ -21,6 +34,7 @@ public class EndInteraction : MonoBehaviour
         if (active && collision.gameObject.tag == "Player")
         {
             prompt.enabled = false;
+            inRange = false;
         }
     }
 }
